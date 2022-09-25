214bf811f4e7c524f5cb3f5cf6e9cf016d359181
jmontero <jmdeamuedo@gmail.com>
2022-09-25 10:15:52 +0200

    test

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET api data. */
-app.get("/url", (req, res, next) => {
+router.get('/', function(req, res, next) {
   res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
 module.exports = router;
+
+
